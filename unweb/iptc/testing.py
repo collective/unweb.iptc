92546a12a6b221aec78766a4f162ea84501d28e6
hvelarde <hector.velarde@gmail.com>
2015-06-16 21:58:45 -0300

    Refactor code as required by code analysis tools
    - Add coding magic comment
    - Nuke trail spaces
    - Sort imports
    - Prefer single quotes
    - Flake8

--- unweb/iptc/testing.py
+++ unweb/iptc/testing.py
@@ -1,14 +1,11 @@
-from Products.CMFCore.utils import getToolByName
-
-from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import applyProfile
-from plone.app.testing import PLONE_FIXTURE
-from plone.app.testing import IntegrationTesting
+# -*- coding: utf-8 -*-
 from plone.app.testing import FunctionalTesting
-
-from plone.testing import z2
-
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from Products.CMFCore.utils import getToolByName
 from zope.configuration import xmlconfig
+
 
 class UnwebIPTC(PloneSandboxLayer):
 
@@ -21,12 +18,12 @@ class UnwebIPTC(PloneSandboxLayer):
     USER_WITH_FULLNAME_PASSWORD = 'secret'
     MANAGER_USER_NAME = 'manager'
     MANAGER_USER_PASSWORD = 'secret'
-    
+
     def setUpZope(self, app, configurationContext):
         # Load ZCML
         import unweb.iptc
-        xmlconfig.file('configure.zcml', 
-                       unweb.iptc, 
+        xmlconfig.file('configure.zcml',
+                       unweb.iptc,
                        context=configurationContext)
 
     def setUpPloneSite(self, portal):
@@ -44,9 +41,9 @@ class UnwebIPTC(PloneSandboxLayer):
             ['Member'],
             [],
         )
-        mtool = getToolByName(portal, 'portal_membership', None) 
-        mtool.addMember('jim', 'Jim', ['Member'], []) 
-        mtool.getMemberById('jim').setMemberProperties({"fullname": 'Jim Fult\xc3\xb8rn'})
+        mtool = getToolByName(portal, 'portal_membership', None)
+        mtool.addMember('jim', 'Jim', ['Member'], [])
+        mtool.getMemberById('jim').setMemberProperties({'fullname': 'Jim Fult\xc3\xb8rn'})
 
         acl_users.userFolderAddUser(
             self.MANAGER_USER_NAME,
@@ -57,9 +54,8 @@ class UnwebIPTC(PloneSandboxLayer):
 
 UNWEB_IPTC_FIXTURE = UnwebIPTC()
 UNWEB_IPTC_INTEGRATION_TESTING = IntegrationTesting(
-    bases=(UNWEB_IPTC_FIXTURE,), 
-    name="UnwebIPTC:Integration")
+    bases=(UNWEB_IPTC_FIXTURE,),
+    name='UnwebIPTC:Integration')
 UNWEB_IPTC_FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(UNWEB_IPTC_FIXTURE,), 
-    name="UnwebIPTC:Functional")
-
+    bases=(UNWEB_IPTC_FIXTURE,),
+    name='UnwebIPTC:Functional')
